0f86970b14063d220fb69fe6d1e3bc9a25965abd
ubakry <ubakry94@gmail.com>
2019-03-22 11:46:47 +0200

    Creating pyomix.py

---
+++ pyomix.py
@@ -0,0 +1,8 @@
+##########################################################################
+#  - PyOmiX - Analysis Workflow                                          #
+#  - Python Script                                                       #
+#  - Mar 22.2019                                                         #
+#  - Copyright: Ahmed Omar, Mohamed Magdy, Usama Bakry, and Waleed Amer  #
+#  - Nile University                                                     #
+##########################################################################
+
