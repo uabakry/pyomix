26c289b089296c631b3162bf94b183ce05d7d23d
ubakry <ubakry94@gmail.com>
2019-04-01 20:40:03 +0200

    - Adding get_args() function. - Linking makdir() with the arguments

--- pyomix.py
+++ pyomix.py
@@ -1,19 +1,48 @@
 ##########################################################################
 #  - PyOmiX - Analysis Workflow                                          #
 #  - Python Script                                                       #
-#  - Mar 22.2019                                                         #
+#  - April 1,2019                                                        #
 #  - Copyright: Ahmed Omar, Mohamed Magdy, Usama Bakry, and Waleed Amer  #
 #  - Nile University                                                     #
 ##########################################################################
 
 # Importing libraries
 import os
+import argparse
 
+args = None
+
+# ----------------------------------------------------------------------
+def get_args():
+    """"""
+    parser = argparse.ArgumentParser(
+        description="PyOmiX - Analysis WorkFlow",
+        epilog="This is where you might put example usage"
+    )
+
+    # required argument
+    parser.add_argument('-i', action="store", required=True, help='Swiss-Prot ids txt file directory')
+
+    # optional arguments
+    parser.add_argument('-o', action="store", help='The output directory', default='.')
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+
+    arguments = vars(parser.parse_args())
+    return arguments
+# ----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    args = get_args()
 
 # Function to make directories for swiss-prot ids
+# ----------------------------------------------------------------------
 def makdirs(file):
-    os.mkdir(os.getcwd() + "/working-dir", int(0o755))
-    os.chdir(os.getcwd()+"/working-dir")
-    print(os.getcwd())
+    os.mkdir(args['o'] + "/pymoix-results", int(0o755))
+    os.chdir(args['o'] + "/pymoix-results")
     for line in file:
-        os.mkdir(os.getcwd()+"/"+line,int(0o755))
+        os.mkdir(str(os.getcwd())+"/"+line.rstrip("\n"), int(0o755))
+# ----------------------------------------------------------------------
+
+file = open(args['i'])
+makdirs(file)
+file.close()
