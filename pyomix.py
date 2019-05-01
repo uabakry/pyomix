1ee698a5562dfe56131c04a2a154fdf9fa097c50
ubakry <ubakry94@gmail.com>
2019-05-01 15:50:59 +0200

    code optimization

--- pyomix.py
+++ pyomix.py
@@ -6,9 +6,11 @@
 #  - Nile University                                                     #
 ##########################################################################
 
-## project Description ..
-print('PyOmiX v0.1 | by Ahmed Omar, Mohamed Magdy, Usama Bakry and Waleed Amer\n'
-      , 'Check https://github.com/ubakry/pyomix for updates.\n')
+## project Description
+print('========================================================================')
+print('PyOmiX v1.0 | by Ahmed Omar, Mohamed Magdy, Usama Bakry and Waleed Amer.')
+print('Check https://github.com/ubakry/pyomix for updates.')
+print('========================================================================')
 
 # ----------------------------------------------------------------------
 # Importing libraries
@@ -24,83 +26,115 @@ args = None
 
 # ----------------------------------------------------------------------
 def get_args():
-    """"""
-    parser = argparse.ArgumentParser(
-        description="PyOmiX - Analysis WorkFlow",
-        epilog="This is where you might put example usage"
-    )
+	""""""
+	parser = argparse.ArgumentParser(
+		description="PyOmiX - Analysis WorkFlow",
+		epilog="This is where you might put example usage"
+	)
 
-    # required argument
-    parser.add_argument('-i', action="store", required=True, help='Swiss-Prot ids txt file directory')
-    parser.add_argument('-d', action="store", required=True, help='fasta file well be used as refrance for alignment')
+	# required argument
+	parser.add_argument('-i', action="store", required=True, help='Swiss-Prot ids txt file directory')
+	parser.add_argument('-d', action="store", required=True, help='fasta file well be used as refrance for alignment')
 
-    # optional arguments
-    parser.add_argument('-o', action="store", help='The output directory', default='.')
-    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+	# optional arguments
+	parser.add_argument('-o', action="store", help='The output directory', default='.')
+	parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
-    arguments = vars(parser.parse_args())
-    return arguments
+	arguments = vars(parser.parse_args())
+	return arguments
 # ----------------------------------------------------------------------
-
-if __name__ == '__main__':
-    args = get_args()
 
 # ----------------------------------------------------------------------
 # Creating reference database for alignment using DIAMOND (makedb)
 # ----------------------------------------------------------------------
-print('create refrance database for alignment using diamond (makedb)')
-make_db = ['diamond', 'makedb', '--in', args['d'], '-d', 'db']
-make_db_proc = subprocess.Popen(make_db, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def makedb():
+	print('create refrance database for alignment using diamond (makedb)')
+	make_db = ['diamond', 'makedb', '--in', args['d'], '-d', 'db']
+	make_db_proc = subprocess.Popen(make_db, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# check the process
-stdout, stderr = make_db_proc.communicate()
-print(stderr.decode())
-
+	# check the process
+	stdout, stderr = make_db_proc.communicate()
+	print(stderr.decode())
 # ----------------------------------------------------------------------
-# Function to make directories for SWISS-Prot ids
-# ----------------------------------------------------------------------
-def makdirs(file):
-    os.mkdir(args['o'] + "/pymoix-results", int(0o755))
-    os.chdir(args['o'] + "/pymoix-results")
-    for line in file:
-        os.mkdir(str(os.getcwd())+"/"+line.rstrip("\n"), int(0o755))
-# ----------------------------------------------------------------------
-
-file = open(args['i'])
-makdirs(file)
-file.close()
-
-# ----------------------------------------------------------------------
-# Function for alignment using DIAMOND
-# ----------------------------------------------------------------------
-
-def diamond_align(fasta):
-    # alignment using blastp
-    diamond = ['diamond', 'blastp', '-q', fasta, '-d', 'db.dmnd', '-f', '6', 'qseqid', '-o', 'id_list.fa']
-    diamond_proc = subprocess.Popen(diamond, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = diamond_proc.communicate()
-    print(stderr.decode())
 
 # ----------------------------------------------------------------------
 # Function to download FASTA File from UniProt and NCBI
 # ----------------------------------------------------------------------
-
 def getfasta(id):
-    if re.search(r"[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}", id):
-        parameters = {"query": id, "format": "fasta"}
-        fasta = requests.get(
-            "https://www.uniprot.org/uniprot/", params=parameters)
-        fastafile = open(id + ".fasta", "w")
-        fastafile.write(fasta.text)
-        fastafile.close()
-    elif re.search(r"[A-Z][0-9]{5}[.][0-9]|[A-Z]{2}[A-Z_][0-9]{5,9}[.][0-9]", id):
-        parameters = {"db": "protein", "id": id,
-                      "rettype": "fasta", "retmode": "text"}
-        fasta = requests.get(
-            "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi", params=parameters)
-        fastafile = open(id + ".fasta", "w")
-        fastafile.write(fasta.text)
-        fastafile.close()
-    else:
-        print ("Wrong ID .. Check your IDs list")
-            
+	if re.search(r"[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}", id):
+		parameters = {"query": id, "format": "fasta"}
+		fasta = requests.get(
+			"https://www.uniprot.org/uniprot/", params=parameters)
+		fastafile = open(id + ".fasta", "w")
+		fastafile.write(fasta.text)
+		fastafile.close()
+	elif re.search(r"[A-Z][0-9]{5}[.][0-9]|[A-Z]{2}[A-Z_][0-9]{5,9}[.][0-9]", id):
+		parameters = {"db": "protein", "id": id,
+					  "rettype": "fasta", "retmode": "text"}
+		fasta = requests.get(
+			"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi", params=parameters)
+		fastafile = open(id + ".fasta", "w")
+		fastafile.write(fasta.text)
+		fastafile.close()
+	else:
+		print ("Wrong ID .. Check your IDs list")
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Function to make directories for SWISS-Prot ids
+# ----------------------------------------------------------------------
+def makdirs(file,parent_dir="/pymoix-results"):
+	if parent_dir == "/pymoix-results":
+		os.mkdir(args['o'] + parent_dir, int(0o755))
+		os.chdir(args['o'] + parent_dir)
+		for id in file:
+			os.mkdir(str(os.getcwd())+"/"+id.rstrip("\n"), int(0o755))
+			os.chdir(str(os.getcwd())+"/"+id.rstrip("\n"))
+			getfasta(id.rstrip("\n"))
+			os.chdir(args['o'] + parent_dir)
+	else:
+		os.mkdir(args['o'] +"/pymoix-results"+ parent_dir+"/"+"align_accessions", int(0o755))
+		os.chdir(args['o'] +"/pymoix-results"+ parent_dir+"/"+"align_accessions")
+		for id in file:
+			getfasta(id.rstrip("\n"))
+		os.chdir(args['o'] +"/pymoix-results")
+	
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Function for alignment using DIAMOND
+# ----------------------------------------------------------------------
+# def diamond_align(fasta):
+# 	# alignment using blastp
+# 	diamond = ['diamond', 'blastp', '-q', fasta, '-d', 'db.dmnd', '-f', '6', 'qseqid', '-o', args['o'] +"/pymoix-results"+#####+'id_list.txt']
+# 	diamond_proc = subprocess.Popen(diamond, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# 	stdout, stderr = diamond_proc.communicate()
+# 	print(stderr.decode())
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Function to merge multiple fasta files in one fasta file.
+# ----------------------------------------------------------------------
+def merge():
+	pass
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Function to perform multiple sequence alignment and get a phylogenetic tree.
+# ----------------------------------------------------------------------
+def clustalo():
+	pass
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Main function
+# ----------------------------------------------------------------------
+def main():
+	file = open(args['i'])
+	makdirs(file)
+	file.close()
+# ----------------------------------------------------------------------
+		
+if __name__ == '__main__':
+	args = get_args()
+	main()
